fa8140b0eac509374f1a172eaf3db47332f860f9
Rey Millares at Home <rsbgm@rcpixel.com>
2019-07-21 02:25:30 +0800

    Updaget on mailgun test controller

--- application/controllers/mailgun/test/Test.php
+++ application/controllers/mailgun/test/Test.php
@@ -17,28 +17,57 @@ class Test extends CI_Controller {
 		$this->load->view('test/test');
 	}
 	function sendmail(){
+		$this->config->load('api');
+		$campaignList = array(
+			'campaign1' => array(
+				'subject'=> 'Campaign 1',
+				'message'=> 'Email sending number 001'
+			),
+			'campaign2' => array(
+				'subject'=> 'Campaign 2',
+				'message'=> 'Email sending number 002'
+			),
+			'campaign3' => array(
+				'subject' => 'Campaign 3',
+				'message' => 'Email sending number 003'
+			),
+			'campaign4' => array(
+				'subject' => 'Campaign 4',
+				'message' => 'Email sending number 004'
+			),
+			'campaign5' => array(
+				'subject' => 'Campaign 5',
+				'message' => 'Email sending number 005'
+			),
+		);
 		if(isset($_POST)){
-			print_r($_POST);
-			$array_data = array(
-		'from'=> ' expertcoder04@gmail.com',
-		'to'=>'developer.ranjan88@gmail.com',
-		'subject'=>$_POST['name'],
-		//'html'=>$html,
-		'text'=>$_POST['message'],
-    );
-    $session = curl_init('https://api.mailgun.net/v3/sandbox2eed136e964a4da6bdc8cb8761d8a85d.mailgun.org/messages');
-    curl_setopt($session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-  	//curl_setopt($session, CURLOPT_USERPWD, 'api:39f733fa0bb3e120eab364505ae06692-afab6073-9c4e4342');
-    curl_setopt($session, CURLOPT_POST, true);
-    curl_setopt($session, CURLOPT_POSTFIELDS, $array_data);
-    curl_setopt($session, CURLOPT_HEADER, false);
-    curl_setopt($session, CURLOPT_ENCODING, 'UTF-8');
-    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
-    $response = curl_exec($session);
-    curl_close($session);
-    $results = json_decode($response, true);
-    print_r($results);
+			foreach($campaignList as $campaign){	
+				$array_data = array(
+					'from'=> 'expertcoder04@gmail.com',
+					'to'=>'rsbgm@rcpixel.com,rsbgm@instylenewyork.com,developer.ranjan88@gmail.com',
+					'subject'=>$campaign['subject'],
+				//'html'=>$html,
+					'text'=>$campaign['message'],
+				);
+			
+				$domain = $this->config->item('mailgun_domain');
+				$key = $this->config->item('mailgun_api');
+				
+				
+				$session = curl_init($domain.'/messages');
+				curl_setopt($session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+				curl_setopt($session, CURLOPT_USERPWD, 'api:'.$key);
+				curl_setopt($session, CURLOPT_POST, true);
+				curl_setopt($session, CURLOPT_POSTFIELDS, $array_data);
+				curl_setopt($session, CURLOPT_HEADER, false);
+				curl_setopt($session, CURLOPT_ENCODING, 'UTF-8');
+				curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
+				$response = curl_exec($session); 
+				curl_close($session);  
+				$results = json_decode($response, true);
+				print_r($results);
+			}
 		}
 	}
 }
