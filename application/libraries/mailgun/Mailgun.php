46ea3fddaa5246ccb2aaba6d63b7e7781cfb763b
Rey Millares at Home <rsbgm@rcpixel.com>
2019-08-02 00:16:33 +0800

    Merge Hardeep mailgun library class

--- application/libraries/mailgun/Mailgun.php
+++ application/libraries/mailgun/Mailgun.php
@@ -1,141 +1,89 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-declare(strict_types=1);
-
-/*
- * Copyright (C) 2013 Mailgun
- *
- * This software may be modified and distributed under the terms
- * of the MIT license. See the LICENSE file for details.
- */
-
-namespace Mailgun;
-
-use Http\Client\Common\PluginClient;
-use Mailgun\HttpClient\HttpClientConfigurator;
-use Mailgun\HttpClient\Plugin\History;
-use Mailgun\HttpClient\RequestBuilder;
-use Mailgun\Hydrator\ModelHydrator;
-use Mailgun\Hydrator\Hydrator;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Client\ClientInterface;
-
-/**
- * This class is the base class for the Mailgun SDK.
- */
 class Mailgun
 {
-    /**
-     * @var string|null
-     */
-    private $apiKey;
+    private $CI;
+	private $key;
+	private $domain;
+	
 
-    /**
-     * @var ClientInterface|PluginClient
-     */
-    private $httpClient;
-
-    /**
-     * @var Hydrator
-     */
-    private $hydrator;
-
-    /**
-     * @var RequestBuilder
-     */
-    private $requestBuilder;
-
-    /**
-     * This is a object that holds the last response from the API.
-     *
-     * @var History
-     */
-    private $responseHistory;
-
-    public function __construct(
-        HttpClientConfigurator $configurator,
-        Hydrator $hydrator = null,
-        RequestBuilder $requestBuilder = null
-    ) {
-        $this->requestBuilder = $requestBuilder ?: new RequestBuilder();
-        $this->hydrator = $hydrator ?: new ModelHydrator();
-
-        $this->httpClient = $configurator->createConfiguredClient();
-        $this->apiKey = $configurator->getApiKey();
-        $this->responseHistory = $configurator->getResponseHistory();
+    public function __construct()
+    {		
+        $this->CI = get_instance();
+		$this->CI->config->load('api');
+		$this->domain = $this->CI->config->item('mailgun_domain');
+		$this->key = $this->CI->config->item('mailgun_api');		
     }
 
-    public static function create(string $apiKey, string $endpoint = 'https://api.mailgun.net'): self
-    {
-        $httpClientConfigurator = (new HttpClientConfigurator())
-            ->setApiKey($apiKey)
-            ->setEndpoint($endpoint);
-
-        return new self($httpClientConfigurator);
+    public function SendMail($to,$messageList,$ishtml = true)
+    { 
+		$to= implode(',', $to);
+		if(count($messageList)==1){
+			$this->sendSingleMail($to,$messageList['message'],$ishtml = true);
+		}
+		if(count($messageList)>1){			
+			$this->sendMultipleMails($to,$messageList,$ishtml = true);
+		}
     }
-
-    public function getLastResponse(): ?ResponseInterface
-    {
-        return $this->responseHistory->getLastResponse();
-    }
-
-    public function attachment(): Api\Attachment
-    {
-        return new Api\Attachment($this->httpClient, $this->requestBuilder, $this->hydrator);
-    }
-
-    public function domains(): Api\Domain
-    {
-        return new Api\Domain($this->httpClient, $this->requestBuilder, $this->hydrator);
-    }
-
-    public function emailValidation(): Api\EmailValidation
-    {
-        return new Api\EmailValidation($this->httpClient, $this->requestBuilder, $this->hydrator);
-    }
-
-    public function events(): Api\Event
-    {
-        return new Api\Event($this->httpClient, $this->requestBuilder, $this->hydrator);
-    }
-
-    public function ips(): Api\Ip
-    {
-        return new Api\Ip($this->httpClient, $this->requestBuilder, $this->hydrator);
-    }
-
-    public function mailingList(): Api\MailingList
-    {
-        return new Api\MailingList($this->httpClient, $this->requestBuilder, $this->hydrator);
-    }
-
-    public function messages(): Api\Message
-    {
-        return new Api\Message($this->httpClient, $this->requestBuilder, $this->hydrator);
-    }
-
-    public function routes(): Api\Route
-    {
-        return new Api\Route($this->httpClient, $this->requestBuilder, $this->hydrator);
-    }
-
-    public function suppressions(): Api\Suppression
-    {
-        return new Api\Suppression($this->httpClient, $this->requestBuilder, $this->hydrator);
-    }
-
-    public function stats(): Api\Stats
-    {
-        return new Api\Stats($this->httpClient, $this->requestBuilder, $this->hydrator);
-    }
-
-    public function tags(): Api\Tag
-    {
-        return new Api\Tag($this->httpClient, $this->requestBuilder, $this->hydrator);
-    }
-
-    public function webhooks(): Api\Webhook
-    {
-        return new Api\Webhook($this->httpClient, $this->requestBuilder, $this->hydrator, $this->apiKey);
-    }
+	private function sendSingleMail($to,$messageList,$ishtml = true){
+		//TODO handle $to elements to start batch processing incase $to exceeds limit.
+ 
+		$array_data = array(
+			'from'=> 'expertcoder04@gmail.com',
+			'to'=>$to,//'developer.ranjan88@gmail.com',//rsbgm@rcpixel.com,rsbgm@instylenewyork.com,
+			'subject'=>$messageList['subject'],
+		);
+		if($ishtml){
+			$array_data['html']=$messageList['message'];
+		}else{
+			$array_data['text']=$messageList['message'];
+		}
+		
+		$session = curl_init($this->domain.'/messages');
+		$this->key;
+		$this->domain.'/messages'; 
+		curl_setopt($session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($session, CURLOPT_USERPWD, 'api:'.$this->key);
+		curl_setopt($session, CURLOPT_POST, true);
+		curl_setopt($session, CURLOPT_POSTFIELDS, $array_data);
+		curl_setopt($session, CURLOPT_HEADER, false);
+		curl_setopt($session, CURLOPT_ENCODING, 'UTF-8');
+		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
+		$response = curl_exec($session); 
+		curl_close($session);  
+		$results = json_decode($response, true);
+		print_r($results); 
+			
+	}
+	private function sendMultipleMails($to,$messageList,$ishtml = true){
+		//TODO handle $to elements to start batch processing incase $to exceeds limit.
+		foreach($messageList as $campaign){	
+			$array_data = array(
+				'from'=> 'expertcoder04@gmail.com',
+				'to'=>$to,//'developer.ranjan88@gmail.com',//rsbgm@rcpixel.com,rsbgm@instylenewyork.com,
+				'subject'=>$campaign['subject'], 
+			);
+			if($ishtml){
+				$array_data['html']=$campaign['message'];
+			}else{
+				$array_data['text']=$campaign['message'];
+			}
+			
+			$session = curl_init($this->domain.'/messages');
+			curl_setopt($session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+			curl_setopt($session, CURLOPT_USERPWD, 'api:'.$this->key);
+			curl_setopt($session, CURLOPT_POST, true);
+			curl_setopt($session, CURLOPT_POSTFIELDS, $array_data);
+			curl_setopt($session, CURLOPT_HEADER, false);
+			curl_setopt($session, CURLOPT_ENCODING, 'UTF-8');
+			curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
+			$response = curl_exec($session); 
+			curl_close($session);  
+			$results = json_decode($response, true);
+			print_r($results); 
+		}		
+	}
 }
