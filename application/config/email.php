a91db363b4bd8e6fb1b41c9ffd190f3aa8e38a46
Rey Millares at Home <rsbgm@rcpixel.com>
2019-08-07 02:53:29 +0800

    Removing config folder from repo

--- application/config/email.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$config['protocol'] = 'mail';
-	$config['charset'] = 'utf-8';
-	$config['wordwrap'] = TRUE;
-	$config['mailtype'] = 'html';
-	
-/* End of file email.php */
-/* Location: ./application/config/email.php */
--- application/config/grecaptcha.php
+++
@@ -1,8 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('ERROR 404: Page not found');
-
-/**
- * Google Re-captcha keys
- */
-
-$config['re_site_key']		= '6LeEdyUUAAAAAEArgxmtAPm8nsdpq7pYcxyHOfG6';
-$config['re_secret_key']	= '6LeEdyUUAAAAAHDhHJVbKvpNjJXGm4iZkjzQzNPV';
