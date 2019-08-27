ab189d57bc53e03131f5fbac8e6025194246ef44
Rey Millares at Home <rsbgm@rcpixel.com>
2019-08-27 17:13:15 +0800

    Update on Mailgun API Class

--- application/libraries/mailgun/Mailgun.php
+++ application/libraries/mailgun/Mailgun.php
@@ -3,120 +3,174 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mailgun
 {
-    private $CI;
-	private $key;
-	private $domain;
-	public $to,$subject,$message, $messageList = array(),$ishtml = true;
-	
+    /**
+	 * Email header FROM - required
+     *
+     * Formats:
+     *      you@your_domain.com
+     *      Your Name <you@your_domain.com>
+	 *
+	 * @var	string
+	 */
+    public $from = '';
 
+    /**
+	 * Email header TO, CC, BCC
+     *
+     * Formats:
+     *      you@your_domain.com
+     *      Your Name <you@your_domain.com>
+     * Can be comma separated for multiple addresses
+	 *
+	 * @var	string
+	 */
+    public $to;
+    public $cc;
+    public $bcc;
+
+    /**
+	 * Email Subject
+	 *
+	 * @var	string
+	 */
+	public $subject;
+
+    /**
+	 * Email Message
+	 *
+	 * @var	string
+	 */
+	public $message;
+
+    /**
+	 * Error Message
+	 *
+	 * @var	string
+	 */
+	public $error_message;
+
+    /**
+	 * Mailgun Text/HTML params
+	 *
+	 * @var	boolean
+	 */
+	public $ishtml = true;
+
+    /**
+	 * Mailgun API Key
+	 *
+	 * @var	string
+	 */
+	protected $key;
+
+    /**
+	 * Mailgun API Domain
+	 *
+	 * @var	string
+	 */
+	protected $domain;
+
+
+    /**
+	 * CI Instance
+	 *
+	 * @var	object
+	 */
+	protected $CI;
+
+    /**
+	 * Constructor
+	 *
+	 * @return	void
+	 */
     public function __construct()
-    {		
+    {
         $this->CI = get_instance();
-		$this->CI->config->load('api');
+
+        // set some default properties
 		$this->domain = $this->CI->config->item('mailgun_domain');
-		$this->key = $this->CI->config->item('mailgun_api');		
+		$this->key = $this->CI->config->item('mailgun_api');
+
+        log_message('info', 'Mailgun Class Loaded');
     }
+
+    // ----------------------------------------------------------------------
 
     public function Send()
-    {	
-	//echo $this->message; 
-		//foreach($this->messageList as $campaign){	
-		$array_data = array(
-			'from'=> 'expertcoder04@gmail.com',
-			'to'=>$this->to,//'developer.ranjan88@gmail.com',//rsbgm@rcpixel.com,rsbgm@instylenewyork.com,
-			'subject'=>$this->subject,
-		);
-		if($this->ishtml){
-			$array_data['html']=$this->message;
-		}else{
-			$array_data['text']=$this->message;
-		}		
-		$session = curl_init($this->domain.'/messages');
-		//$this->key;
-		//$this->domain.'/messages'; 
-		curl_setopt($session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-		curl_setopt($session, CURLOPT_USERPWD, 'api:'.$this->key);
-		curl_setopt($session, CURLOPT_POST, true);
-		curl_setopt($session, CURLOPT_POSTFIELDS, $array_data);
-		curl_setopt($session, CURLOPT_HEADER, false);
-		curl_setopt($session, CURLOPT_ENCODING, 'UTF-8');
-		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
-		$response = curl_exec($session); 
-		curl_close($session);  
-		$results = json_decode($response, true);
-		print_r($results); 
-		//}
-		// if(count($messageList)==1){
-			// $this->sendSingleMail($to,$messageList['message'],$ishtml = true);
-		// }
-		// if(count($messageList)>1){			
-			// $this->sendMultipleMails($to,$messageList,$ishtml = true);
-		//}
-    }
-	public function clear(){
-		foreach ($this as $key => $value) {
-            unset($this->$key);
+    {
+        if (
+            ! $this->from
+            OR ! $this->to
+        )
+        {
+            // nothing more to do...
+            $this->error_message = 'FROM/TO field is required.'
+
+            return FALSE;
         }
-	}
-	private function sendSingleMail($to,$messageList,$ishtml = true){
-		//TODO handle $to elements to start batch processing incase $to exceeds limit.
- 
-		$array_data = array(
-			'from'=> 'expertcoder04@gmail.com',
-			'to'=>$to,//'developer.ranjan88@gmail.com',//rsbgm@rcpixel.com,rsbgm@instylenewyork.com,
-			'subject'=>$messageList['subject'],
-		);
-		if($ishtml){
-			$array_data['html']=$messageList['message'];
-		}else{
-			$array_data['text']=$messageList['message'];
+
+        if ( ! $this->message)
+        {
+            // nothing more to do...
+            $this->error_message = 'MESSAGE field is required.'
+
+            return FALSE;
+        }
+
+        // some non-required items
+        if ($this->cc) $params['cc'] = $this->cc;
+        if ($this->bcc) $params['bcc'] = $this->bcc;
+
+        // set main email header params accordingly
+        $params['from'] = $this->from;
+        $params['to'] = $this->to;
+        $params['subject'] = $this->subject;
+
+        // the message
+		if ($this->ishtml)
+        {
+			$params['html'] = $this->message;
 		}
-		
-		$session = curl_init($this->domain.'/messages');
-		$this->key;
-		$this->domain.'/messages'; 
-		curl_setopt($session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-		curl_setopt($session, CURLOPT_USERPWD, 'api:'.$this->key);
-		curl_setopt($session, CURLOPT_POST, true);
-		curl_setopt($session, CURLOPT_POSTFIELDS, $array_data);
-		curl_setopt($session, CURLOPT_HEADER, false);
-		curl_setopt($session, CURLOPT_ENCODING, 'UTF-8');
-		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
-		$response = curl_exec($session); 
-		curl_close($session);  
-		$results = json_decode($response, true);
-		print_r($results); 
-			
-	}
-	private function sendMultipleMails($to,$messageList,$ishtml = true){
-		//TODO handle $to elements to start batch processing incase $to exceeds limit.
-		foreach($messageList as $campaign){	
-			$array_data = array(
-				'from'=> 'expertcoder04@gmail.com',
-				'to'=>$to,//'developer.ranjan88@gmail.com',//rsbgm@rcpixel.com,rsbgm@instylenewyork.com,
-				'subject'=>$campaign['subject'], 
-			);
-			if($ishtml){
-				$array_data['html']=$campaign['message'];
-			}else{
-				$array_data['text']=$campaign['message'];
-			}
-			
-			$session = curl_init($this->domain.'/messages');
-			curl_setopt($session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-			curl_setopt($session, CURLOPT_USERPWD, 'api:'.$this->key);
-			curl_setopt($session, CURLOPT_POST, true);
-			curl_setopt($session, CURLOPT_POSTFIELDS, $array_data);
-			curl_setopt($session, CURLOPT_HEADER, false);
-			curl_setopt($session, CURLOPT_ENCODING, 'UTF-8');
-			curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
-			$response = curl_exec($session); 
-			curl_close($session);  
-			$results = json_decode($response, true);
-			print_r($results); 
-		}		
-	}
+        else
+        {
+			$params['text']=$this->message;
+		}
+
+        // let do the curl
+        $session = curl_init($this->domain.'/messages');
+
+        // set settings
+        curl_setopt($session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($session, CURLOPT_USERPWD, 'api:'.$this->key);
+        curl_setopt($session, CURLOPT_POST, true);
+        curl_setopt($session, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($session, CURLOPT_HEADER, false);
+        curl_setopt($session, CURLOPT_ENCODING, 'UTF-8');
+        curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
+
+        // get response
+        $response = curl_exec($session);
+
+        // close curl
+        curl_close($session);
+
+        $results = json_decode($response, true);
+
+        if ($results["message"] == "Queued. Thank you.")
+        {
+            // successful
+        	return TRUE;
+        }
+        else
+        {
+            // check the error
+            $this->error_message = $results["message"];
+
+        	return FALSE;
+        }
+    }
+
+    // ----------------------------------------------------------------------
+
 }
